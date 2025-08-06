fbcf246a5123eebcf8d8cb06af32b4172557e96b
MrAbelDev <abelmruso@gmail.com>
2025-08-06 17:43:31 +0200

    comienzo parte 2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const http = require("http");
 const app = require("./app");
 
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Conectado a MongoDB Atlas"))
+    .catch((error) => console.error("Error al conectar a MongoDB:", error));
+
 const normalizePort = (val) => {
     const port = parseInt(val, 10);
 
