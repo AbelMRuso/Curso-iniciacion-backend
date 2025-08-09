61e4bdb8b0844f49ec3af9e4c5c1eb591c7d8887
MrAbelDev <abelmruso@gmail.com>
2025-08-09 20:18:21 +0200

    separacion de rutas y logica de rutas en 2 carpetas diferentes

---
+++ backend/controllers/stuff.js
@@ -0,0 +1,38 @@
+//STUFF DENTRO DE CONTROLLERS
+
+const Thing = require("../models/thing");
+
+exports.createThing = (req, res, next) => {
+    delete req.body._id; //borra el id enviado por el front para substituirlo por el que se creará automáticamente en el back
+    const thing = new Thing({
+        ...req.body,
+    });
+    thing
+        .save() //guarda los datos del objeto Thing
+        .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+        .catch((error) => res.status(400).json({ error }));
+};
+
+exports.modifyThing = (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id }) // updateOne actualiza el objeto cuyo id se corresponde con el id de la url
+        .then(() => res.status(200).json({ message: "Objet modifié !" }))
+        .catch((error) => res.status(400).json({ error }));
+};
+
+exports.deleteThing = (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+        .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getOneThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id }) // findOne busca únicamente el objeto cuyo _id coincida con el id de la url
+        .then((thing) => res.status(200).json(thing))
+        .catch((error) => res.status(404).json({ error }));
+};
+
+exports.getAllThings = (req, res, next) => {
+    Thing.find() //busca todos los documentos de la colección asociada al modelo thing en mongoDB
+        .then((things) => res.status(200).json(things)) // si  los encuentra los devuelve como respuesta en formato json
+        .catch((error) => res.status(400).json({ error }));
+};
