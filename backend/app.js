09f874703e91c924597fdbb181f948e98c88273d
MrAbelDev <abelmruso@gmail.com>
2025-08-09 20:46:21 +0200

    logica de registro de usuario

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
 
 app.use(express.json());
 
@@ -16,5 +17,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
