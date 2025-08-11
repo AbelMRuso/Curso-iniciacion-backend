26edb1412a81633c3472a0f3b89f9d2f38539cd7
MrAbelDev <abelmruso@gmail.com>
2025-08-11 20:45:31 +0200

    modificación de modifyThing y createThing para gestionar el uso de ficheros

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const stuffRoutes = require("./routes/stuff");
 const userRoutes = require("./routes/user");
+const path = require("path");
 
 app.use(express.json());
 
@@ -18,5 +19,6 @@ app.use((req, res, next) => {
 
 app.use("/api/stuff", stuffRoutes);
 app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
